fbb1efd2e286910189d792ad2db104908a79cddb
Rockerok <Rockerok@mail.ru>
2022-05-21 22:12:17 +0300

    Start

---
+++ src/main/java/ru/gb/servlet/Product.java
@@ -0,0 +1,11 @@
+package ru.gb.servlet;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+@WebServlet(name = "Cart", urlPatterns = "/cart")
+
+// Создайте класс Product с полями (id, title, cost);
+public class Product extends HttpServlet {
+
+}
